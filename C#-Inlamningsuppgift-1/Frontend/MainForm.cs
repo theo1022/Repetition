ea770b5752299dfd31848e205ab2228cd3549eb8
Theo Olsson <theo1022@hotmail.se>
2023-10-04 20:29:11 +0200

    TODO MainForm Todo answered

--- C#-Inlamningsuppgift-1/Frontend/MainForm.cs
+++ C#-Inlamningsuppgift-1/Frontend/MainForm.cs
@@ -17,6 +17,8 @@ namespace Frontend
     public partial class MainForm : Form
     {
         //TODO Vad är skillnaden på en vanlig metod och en konstruktor som 'MainForm'?
+        // Answer: A constructor is called when an instance of a class is created to initialize the object
+        // A method can be called from an object to execute a block of code to for example modify the object or return a value
         public MainForm()
         {
             InitializeComponent();
