fe081477e65f0423e7f761e0b63047cb917b5f7f
Theo Olsson <theo1022@hotmail.se>
2023-10-10 18:48:45 +0200

    Intial commit C#-Inlamningsuppgift-2 C#-Inlamningsuppgift-2 unchanged

---
+++ C#-Inlamningsuppgift-2/Tests/LoginTestSuite.cs
@@ -0,0 +1,15 @@
+using Core;
+using System;
+using Xunit;
+
+namespace Tests
+{
+    public class LoginTestSuite
+    {
+        [Fact]
+        public void UsernameAndPasswordRegistrationTest()
+        {
+            LoginManager loginManager = new LoginManager();
+        }
+    }
+}
